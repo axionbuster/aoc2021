25eff39335cf10671fc629e17776ec870d743374
YuJin Kim <yujinkim@mines.edu>
2022-01-13 21:07:34 +0000

    Rename 11.01b to 11.01

--- c/11.01.c
+++ c/11.01.c
@@ -1,226 +1,181 @@
 #include <assert.h>
-#include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "lib/dbgprint.h"
 #include "lib/xalloc.h"
 
-#define TEXTBOLD "\e[1m"
-#define TEXTNORMAL "\e[0m"
+#define TEXTBOLD "\033[1m"
+#define TEXTNORMAL "\033[0m"
 
-#define FORRC(rlim, clim)                                                      \
-  for (int r = 0; r < rlim; r++) {                                             \
-    for (int c = 0; c < clim; c++) {
+typedef struct grid_t {
+  // Ordinary numbers (integers), not characters.
+  signed char *grid;
+  int width, height;
+} Grid;
 
-#define ENDFORRC                                                               \
-  }                                                                            \
-  }
+#define FORRC(r, rlim, c, clim)                                                \
+  for (int r = 0; r < rlim; r++)                                               \
+    for (int c = 0; c < clim; c++)
 
-// Note that signed char is treated as ordinary integers, not characters.
-typedef struct strgrid_t {
-  signed char **grid;
-  int ncols;
-  int nrows;
-  int caprows;
-} StringGrid;
+typedef struct cursor_t {
+  int row, column;
+} Cursor;
 
-static StringGrid mkstrgrid() {
-  StringGrid sg = {.ncols = 0, .nrows = 0, .caprows = 1};
-  sg.grid = xcalloc(1, sizeof(signed char *));
-  return sg;
+#define mkcur(r, c) ((Cursor){.row = (r), .column = (c)})
+
+static void initgrid(Grid *g) {
+  g->grid = NULL;
+  g->width = 0;
+  g->height = 0;
 }
 
-static void delstrgrid(StringGrid sg) {
-  for (int row = 0; row < sg.nrows; row++) {
-    free(sg.grid[row]);
-  }
-  free(sg.grid);
-}
+static void delgrid(Grid g) { free(g.grid); }
 
-static void movstrgrid(StringGrid *sg, signed char *row) {
-  if (sg->nrows == sg->caprows) {
-    sg->caprows *= 2;
-    sg->grid = xrealloc(sg->grid, sg->caprows * sizeof(signed char *));
-  }
-  sg->grid[sg->nrows++] = row;
-}
-
-static signed char *charat(StringGrid *sg, int r, int c) {
-  if (r < 0 || r >= sg->nrows)
+static signed char *at(Grid *g, Cursor c) {
+  if (c.column < 0 || c.column >= g->width)
     return NULL;
-  if (c < 0 || c >= sg->ncols)
+  if (c.row < 0 || c.row >= g->height)
     return NULL;
-  return &sg->grid[r][c];
+  return &g->grid[c.column + c.row * g->width];
 }
 
-// Print, alongwith a newline, the number in a line; bold characters '0'.
-static void dbg_print_sg_line(StringGrid *sg, int r) {
-  bool bold = false;
-
-  for (int c = 0; c < sg->ncols; c++) {
-    signed char ch = *charat(sg, r, c);
-    if (ch == 0) {
-      bold = true;
-      dbgprintf(TEXTBOLD);
-    } else if (bold) {
-      dbgprintf(TEXTNORMAL);
+static void dbggrid(Grid *g) {
+#ifndef NDEBUG
+  int tty = isatty(STDERR_FILENO);
+#else
+  int tty = 0;
+#endif
+  char const *normal = tty ? TEXTNORMAL : "";
+  char const *boldtext = tty ? TEXTBOLD : "";
+  signed char ch;
+  bool bold;
+  FORRC(r, g->height, c, g->width) {
+    Cursor co = mkcur(r, c);
+    ch = *at(g, co);
+    if (!c) {
+      if (bold) {
+        dbgprintf(normal);
+      }
+      bold = false;
+      dbgprintf("\n");
     }
-    dbgprintf("%x", ch);
+    if (ch && bold) {
+      bold = false;
+      dbgprintf(normal);
+    }
+    if (!ch && !bold) {
+      bold = true;
+      dbgprintf(boldtext);
+    }
+
+    dbgprintf("%1x", ch);
   }
 
   if (bold) {
-    dbgprintf(TEXTNORMAL);
+    dbgprintf(normal);
   }
   dbgprintf("\n");
 }
 
-static void dbg_print_sg(StringGrid *sg) {
-  for (int r = 0; r < sg->nrows; r++) {
-    dbg_print_sg_line(sg, r);
+// Copy ASCII numeral rows (C-string) into the grid.
+static void cpyrawrow(Grid *g, char const *restrict row) {
+  size_t ll = g->width ? g->width : strlen(row);
+  size_t sz = (size_t)g->width * (size_t)g->height;
+  size_t nsz = sz + ll;
+  g->grid = xrealloc(g->grid, nsz);
+  g->width = g->width ? g->width : ll;
+  g->height++;
+
+  for (int i = 0; i < g->width; i++) {
+    g->grid[sz + i] = (int)row[i] - '0';
+    assert(g->grid[sz + i] >= 0);
+    assert(g->grid[sz + i] <= 9);
   }
-  dbgprintf("\n");
 }
 
-// Implement rule 2 of the description, which is summarized as "increase
-// adjacent members of those who are flashing."
-static void incadj(StringGrid *sg) {
-  // The following code only executes at first call.
-
-  static StringGrid *scratch = NULL;
-
-  if (!scratch) {
-    scratch = xmalloc(sizeof(StringGrid));
-    *scratch = mkstrgrid();
-    for (int rr = 0; rr < sg->nrows; rr++) {
-      signed char *line = xmalloc(sg->ncols);
-      movstrgrid(scratch, line);
-    }
-
-    scratch->ncols = sg->ncols;
-
-    assert(scratch->nrows == sg->nrows);
-    assert(scratch->ncols == sg->ncols);
-    assert(scratch->nrows > 0);
-    assert(scratch->ncols > 0);
+static void incall(Grid *g) {
+  FORRC(r, g->height, c, g->width) {
+    Cursor co = mkcur(r, c);
+    (*at(g, co)) += 1;
   }
+}
 
-  // The subsequent code may execute more than one time.
-
-  // Zero out the update grid.
-  for (int r = 0; r < scratch->nrows; r++) {
-    signed char *row = scratch->grid[r];
-    for (int c = 0; c < scratch->ncols; c++) {
-      row[c] = 0;
-    }
+static void truncall(Grid *g) {
+  FORRC(r, g->height, c, g->width) {
+    Cursor co = mkcur(r, c);
+    signed char *x = at(g, co);
+    *x = (*x > 9 || *x < 0) ? 0 : *x;
   }
+}
 
-  // Do step 2 and 3 of the given rule description.
-  int flashed = 1;
-
-  while (flashed--) {
-    // Loop while any new flashes are generated.
-    // How "nflash--" works:
-    //  1. At first nflash != 0 so the loop is always entered.
-    //     Once it enters the loop, nflash == 0.
-    //  2. If the loop does not touch nflash at all then nflash == 0
-    //     at the next loop. Break (therefore, only when no more flashes occur)
-    //  3. If any flash occurs, and the loop engages again, nflash is reset to 0
-
-    for (int r = 0; r < sg->nrows; r++) {
-      for (int c = 0; c < sg->ncols; c++) {
-        signed char *oc = charat(sg, r, c);
-        if (*oc > 9) {
-          // Increase adjacent.
-          for (int rr = -1; rr <= 1; rr++) {
-            for (int cc = -1; cc <= 1; cc++) {
-              if (rr != 0 && cc != 0) {
-                signed char *relc = charat(scratch, r + rr, c + cc);
-                // Negative values in scratch means that it has flashed before
-                // and therefore must never flash again.
-                if (relc && *relc >= 0)
-                  (*relc) = 1;
-              }
-            }
-          }
-        }
+// For any element who is above 9, increment all adjacent elements by 1.
+// Elements are not truncated. Mark those already flashed with a negative
+// number. Return nonnegative if and only if at least one octopus flashed.
+// Return value is always nonnegative.
+static int flashall(Grid *g) {
+  int flash = 0;
+  FORRC(r, g->height, c, g->width) {
+    signed char *gg = at(g, mkcur(r, c));
+    if (*gg > 9) {
+      flash++;
+      *gg = -1;
+      FORRC(rr, 3, cc, 3) {
+        if (rr == 1 && cc == 1)
+          continue;
+        signed char *mm = at(g, mkcur(r + rr - 1, c + cc - 1));
+        if (mm && *mm >= 0)
+          (*mm)++;
       }
     }
-
-    for (int r = 0; r < sg->nrows; r++) {
-      for (int c = 0; c < sg->ncols; c++) {
-        signed char *oc = charat(sg, r, c);
-        signed char *sc = charat(scratch, r, c);
-        *oc += *sc;
-        if (*oc > 9) {
-          // When a value in scratch is negative, it will never flash again.
-          *sc = -1;
-          flashed = 1;
-        }
-      }
-    }
-
-    // Zero out the update grid.
-    for (int r = 0; r < scratch->nrows; r++) {
-      signed char *row = scratch->grid[r];
-      for (int c = 0; c < scratch->ncols; c++) {
-        row[c] = 0;
-      }
-    }
-    // Debug
-    dbg_print_sg(sg);
   }
+  return flash;
 }
 
 int main(void) {
-  int scan, iline;
+  Grid g;
+  initgrid(&g);
+
   char *line;
-  StringGrid sg = mkstrgrid();
-
-  for (iline = 0; (scan = scanf("%ms ", &line)) != EOF; iline++) {
-    int i = 0;
-    if (iline == 0) {
-      sg.ncols = strlen(line);
-    }
-    signed char *alias = (signed char *)line;
-    for (i = 0; *line != '\0'; i++, line++) {
-      *line = *line - '0';
-    }
-    movstrgrid(&sg, (signed char *)alias);
+  for (int scan = 0; (scan = scanf("%ms ", &line)) != EOF; free(line)) {
+    cpyrawrow(&g, line);
   }
 
-  dbgprintf("Before any steps:\n");
-  for (int r = 0; r < sg.nrows; r++) {
-    dbg_print_sg_line(&sg, r);
-  }
+  dbgprintf("Before any steps:");
+  dbggrid(&g);
   dbgprintf("\n");
 
-  for (int step = 1; step <= 100; step++) {
-    for (int r = 0; r < sg.nrows; r++) {
-      for (int c = 0; c < sg.ncols; c++) {
-        (*charat(&sg, r, c))++;
-      }
+  int all_flash = g.width * g.height;
+  int flash = 0;
+  for (int step = 1;; step++) {
+    int step_flash = 0, local_flash = 0;
+    incall(&g);
+    while ((local_flash = flashall(&g))) {
+      step_flash += local_flash;
     }
-
-    incadj(&sg);
-
-    for (int r = 0; r < sg.nrows; r++) {
-      for (int c = 0; c < sg.ncols; c++) {
-        signed char *pch = charat(&sg, r, c);
-        if (*pch > 9) {
-          *pch = 0;
-        }
-      }
+    flash += step_flash;
+    truncall(&g);
+    if (step <= 10 || step % 10 == 0 || step_flash == all_flash) {
+      dbgprintf("After step %d:", step);
+      dbggrid(&g);
+      dbgprintf("\n");
     }
-
-    if (step <= 10 || step % 10 == 0) {
-      dbgprintf("After step %d:\n", step);
-      dbg_print_sg(&sg);
+    if (step == 100) {
+      dbgflush(stderr);
+      printf("After 100 steps, %d flashes were found.\n", flash);
+      dbgflush(stdin);
+    }
+    if (step_flash == all_flash) {
+      dbgflush(stderr);
+      printf("On step %d, all octopuses flash (%d)\n", step, step_flash);
+      dbgflush(stdin);
+      break;
     }
   }
 
-  delstrgrid(sg);
+  delgrid(g);
   return 0;
 }
