14e54d06dcb6fee2b89065f62c75565a161b1f19
YuJin Kim <yujinkim@mines.edu>
2022-01-22 18:36:18 -0700

    Reformat day 14 in Rust

--- rust/p1402/src/main.rs
+++ rust/p1402/src/main.rs
@@ -8,6 +8,9 @@
 //!
 //! It's very fast (operating under 10 milliseconds), constant space and linear time w.r.t.
 //! number of iterations.
+//!
+//! But I have to say, it really depends on the quality and quantity of the cache in the CPU.
+//!
 
 use std::cmp::{max, min};
 use std::env;
@@ -20,6 +23,9 @@ macro_rules! dbgprint {
     ($($arg:tt)*) => ( if cfg!(debug_assertions) { eprint!($($arg)*) })
 }
 
+/// This is inherited from the C library, though independently implemented.
+/// ATOI converts a Rust string into a 32-bit signed integer if it can be converted;
+/// If it can't, it returns 0.
 fn atoi(s: &str) -> i32 {
     match i32::from_str(s) {
         Ok(n) => n,
@@ -27,21 +33,32 @@ fn atoi(s: &str) -> i32 {
     }
 }
 
+/// The (higher, lower) half-words are converted into a full-word which can be used to index a
+/// slice.
 fn unionize(cc: (u8, u8)) -> usize {
     ((cc.0 as usize) << 8) + cc.1 as usize
 }
 
+/// A full word is converted into two half words, (higher, lower).
 fn separate(uu: usize) -> (u8, u8) {
     ((uu >> 8) as u8, (uu & 0xff) as u8)
 }
 
 struct Problem {
+    /// This maps each pattern (i.e. full word) to their signed count; each value is non-negative.
     pattern_counts: [isize; u16::MAX as usize],
+
+    /// This maps each pattern (i.e. full word) to their ASCII character replacement.
     rewrite_rules: [u8; u16::MAX as usize],
+
+    /// This keeps track of how many of each character exists.
     char_counts: [isize; ('Z' as usize) + 1],
 }
 
 fn main() {
+    // If one argument is provided, it is the number of iterations. Accept that argument.
+    // If no argument is provided, the default is 1 iteration.
+    // If two more more arguments exist or the number of iterations is not positive, error out.
     let mut args = env::args();
     let max_iterations;
     if args.len() == 2 {
@@ -54,6 +71,11 @@ fn main() {
         exit(1);
     }
 
+    // Accept the input:
+    //  1. The line --> converted into patterns
+    //  2. (Blank line)
+    //  3. The rewrite rules
+
     let mut problem = Problem {
         pattern_counts: [0; u16::MAX as usize],
         rewrite_rules: [0; u16::MAX as usize],
@@ -63,7 +85,7 @@ fn main() {
     let line_reader = BufReader::new(stdin());
     let mut lines = line_reader.lines();
     let line: String = lines.next().unwrap().unwrap();
-    let line = line.as_bytes();
+    let line: &[u8] = line.as_bytes();
     for c in line {
         problem.char_counts[*c as usize] += 1;
     }
@@ -72,14 +94,11 @@ fn main() {
         let c2 = c1c2[1];
         problem.pattern_counts[unionize((c1, c2))] += 1;
     }
-
-    // Skip one line.
     lines.next();
-
     for _rewrite in lines.map(|s| s.unwrap()) {
         let (c1c2, cr) = _rewrite.split_once(" -> ").unwrap();
         let c1c2 = c1c2.as_bytes();
-        let c1c2 = (c1c2[0], c1c2[1]);
+        let c1c2: (u8, u8) = (c1c2[0], c1c2[1]);
         let cr = cr.as_bytes();
         problem.rewrite_rules[unionize(c1c2)] = cr[0];
     }
@@ -88,11 +107,14 @@ fn main() {
     // START evolution of the string rewriting system
 
     for iteration in 1..=max_iterations {
+        // In an L-system like this, string replacements are simultaneous.
+        // Since the writes have no dependency among each other, we must separate the writes from
+        // the reads.
         let mut updates = [0 as isize; u16::MAX as usize];
 
         for pattern in 0..u16::MAX as usize {
             if problem.rewrite_rules[pattern] > 0 && problem.pattern_counts[pattern] > 0 {
-                // Once detect a pattern that matches a known rule, do two things:
+                // Once detect a pattern that matches a known rule (thus valid), do two things:
                 //  1. Insert the two other patterns. E.g. Matching XY against XY -> Z creates XZ
                 //     and ZY patterns.
                 //  2. Increment the character count for Z.
@@ -101,6 +123,7 @@ fn main() {
                 let cr = problem.rewrite_rules[pattern];
                 let new_pattern1 = unionize((c1, cr));
                 let new_pattern2 = unionize((cr, c2));
+
                 problem.char_counts[cr as usize] += n;
                 updates[pattern] -= n;
                 updates[new_pattern1] += n;
@@ -108,13 +131,19 @@ fn main() {
             }
         }
 
+        // As mentioned, the read and write are separated due to the stipulation that all string
+        // rewrites happen simultaneously. No dependency is allowed among the rewriting rules.
         for pattern in 0..u16::MAX as usize {
             if updates[pattern] != 0 {
                 problem.pattern_counts[pattern] += updates[pattern];
             }
+
+            // It is very possible that updates has negative elements.
+            // However, pattern_counts is strictly non-negative.
             debug_assert!(problem.pattern_counts[pattern] >= 0);
         }
 
+        // Statistics
         let mut c_most = isize::MIN;
         let mut c_least = isize::MAX;
         for c in ('A' as usize)..('Z' as usize) {
