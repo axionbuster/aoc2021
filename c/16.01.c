6edb70de8b918c7627ce445325a73528dbbe8dea
YuJin Kim <yujinkim@mines.edu>
2022-01-26 06:54:09 +0000

    Accept a literal

--- c/16.01.c
+++ c/16.01.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 
 #include "lib/dbgprint.h"
+#include "lib/iminmax.h"
 #include "lib/xalloc.h"
 
 // Basic data structures
@@ -45,36 +46,228 @@ static bool isxdigit1(int c) {
 
 #define higher(o) ((uint8_t)(o) << 4)
 
-// We actually handle bits.
+// We handle the input bit-by-bit.
 
 typedef struct _parse_t {
   // (str.len / 8) = oct_q ... oct_r;
   // oct_q serves as the octet index; oct_r as the index within that octet.
   int oct_q, oct_r;
-} ParseMachine;
+} Bitdex;
 
-// Slice an octet (little endian)
-static uint8_t lend8sl(ParseMachine *mac, Octets *str, unsigned n_bits) {
-  assert(n_bits > 0);
-  assert(n_bits < 8);
-  // If oct_r + n_bits go 8 or above, we have hit the octet boundary.
-  bool oob = (n_bits + mac->oct_r) > 8;
-
-  if (oob) {
-    // TODO: Go above boundary.
-  } else {
-    // No go above boundary.
-
-    // Get (8 - n_bits) shift right by supposing mac->oct_r == 0.
-    // Then, if mac->oct_r > 0, notice that the amount of right-shifts
-    // decreases.
-    unsigned shift = 8 - n_bits - mac->oct_r;
-
-    uint8_t raw = str->octets[mac->oct_q];
-    raw >>= shift;
-    return raw;
+static void increment_bitdex(Bitdex *b) {
+  if ((b->oct_r += 1) == 8) {
+    b->oct_r = 0;
+    b->oct_q += 1;
   }
 }
+
+typedef enum {
+  FINE = 0,
+  NOGO = 1,
+
+  // This is used to delineate the "end" of something, usually end of packet.
+  STOP = 2
+} ParseCondition;
+
+// This is a global variable that is 0 when no error occurs and not 0 when
+// anything bad has ever occurred. Code cannot set this to 0 (only the user
+// can).
+ParseCondition parse_error;
+
+// Used for debug printing only.
+bool suppress_error_print = false;
+
+// Returns 0 if Octets (o) at bit (b) is 0; 1 otherwise.
+static uint8_t get_bit(Octets *o, Bitdex *b) {
+  assert(b->oct_q >= 0);
+  if (b->oct_q >= o->len) {
+#ifndef NDEBUG
+    if (!suppress_error_print)
+      dbgprintf("parse_error at get_bit: range oob.\n");
+#endif
+    parse_error = NOGO;
+    return 0;
+  }
+  uint8_t octet = o->octets[b->oct_q];
+
+  // The correction 8 - (bit) - 1 had to be made since
+  // the bits are in LE on this machine.
+  uint8_t mask = 1 << (8 - b->oct_r - 1);
+
+  increment_bitdex(b);
+
+  return !!(octet & mask);
+}
+
+static uint8_t accept_version(Octets *o, Bitdex *b) {
+  uint8_t bits[3] = {0};
+
+  bits[0] = get_bit(o, b);
+  bits[1] = get_bit(o, b);
+  bits[2] = get_bit(o, b);
+
+  if (parse_error == FINE) {
+    uint8_t version = (bits[0] << 2) | (bits[1] << 1) | (bits[2]);
+    return version;
+  }
+
+  return 0;
+}
+
+static uint8_t accept_id(Octets *o, Bitdex *b) {
+  // Yeah, the same code is used to parse packet IDs as well as the versions.
+  // Coincidence (see specs).
+  return accept_version(o, b);
+}
+
+static uint8_t accept_nibble(Octets *o, Bitdex *b) {
+  uint8_t bits[5] = {0};
+  for (int i = 0; i < 5; i++) {
+    bits[i] = get_bit(o, b);
+  }
+  if (!parse_error) {
+    if (bits[0] == 0) {
+      parse_error = STOP;
+    }
+    return (bits[1] << 3) | (bits[2] << 2) | (bits[3] << 1) | bits[4];
+  }
+  return 0;
+}
+
+static void discard_bits(Bitdex *b, int n) {
+  for (int i = 0; i < n; i++) {
+    increment_bitdex(b);
+  }
+}
+
+// Accept and return the literal number.
+// Most significant bits get pushed first (lower index).
+static Octets accept_literal(Octets *o, Bitdex *b) {
+  Octets literal = {0};
+
+  // The number of bits in the packet are rounded up to the smallest 4*n.
+  long bits_read = 0;
+
+  if (parse_error != FINE) {
+    return (Octets){0};
+  }
+
+  literal.cap = 1;
+  literal.octets = xcalloc(1, sizeof(uint8_t));
+
+  bool most_sig = true;
+  uint8_t c = 0;
+
+  for (;;) {
+    uint8_t nib = accept_nibble(o, b);
+    if (most_sig) {
+      most_sig = false;
+      c |= higher(nib);
+    } else {
+      most_sig = true;
+      c |= nib;
+      insert_octet(&literal, c);
+      c = 0;
+    }
+    if (parse_error == STOP) {
+      break;
+    }
+  }
+  if (!most_sig) {
+    insert_octet(&literal, c);
+
+    // Correction for nibble misalignment
+    for (int i = literal.len - 1; i > 0; i--) {
+      literal.octets[i] >>= 4;
+      literal.octets[i] |= (literal.octets[i - 1]) << 4;
+    }
+    literal.octets[0] >>= 4;
+  }
+
+  assert(parse_error != FINE);
+  if (parse_error == NOGO) {
+    free(literal.octets);
+    return (Octets){0};
+  } else {
+    return literal;
+  }
+}
+
+// Show an octet
+
+static int clamp(int x, int up, int low) {
+  if (x > up) {
+    return up;
+  } else if (x < low) {
+    return low;
+  }
+  return x;
+}
+
+#ifndef NDEBUG
+static void print_octets(Octets *o) {
+  // I understand that this code is unreadable.
+  // However it works and it's used for debugging.
+
+  // Print the binary and the decimal representations of a potentially
+  // very long series of octets, while supporting truncation at the
+  // most significant 8 octets / 64 bits (lower indices).
+
+  // Example:
+  //  Octet [binary:
+  //  0100001000001101011101001100001100001000100000000100001100111001... ]
+  //   [decimal: (upper 64-bits BE) 4759588762210550585] (675 octets)
+  int parse_error_save = parse_error;
+  parse_error = FINE;
+  suppress_error_print = true;
+  Bitdex index = {0};
+  uint8_t bit = 0;
+  long n_read = 0;
+  dbgprintf("Octet [binary: ");
+  while ((bit = get_bit(o, &index)), !parse_error && n_read < 64) {
+    if (bit)
+      dbgprintf("1");
+    else
+      dbgprintf("0");
+    n_read++;
+  }
+  if (!parse_error) {
+    dbgprintf("... ");
+  }
+  dbgprintf("] [decimal: (upper 64-bits BE) ");
+  bool over64bits = o->len > 8 ? !!o->octets[8] : false;
+  uint64_t low64 = 0;
+  for (int i = 0; i < tg_min(8, o->len); i++) {
+    low64 |= (uint64_t)o->octets[i] << ((8 - i - 1) * 8);
+  }
+  int diff = tg_max(8, o->len) - tg_min(8, o->len);
+  int n_shift = clamp(diff * 8, 63, 0);
+  if (n_shift != 63) {
+    dbgprintf("%lu] ", low64 >> n_shift);
+  } else {
+    dbgprintf("%lu] ", low64);
+  }
+  dbgprintf("(%d octets)\n", o->len);
+  suppress_error_print = false;
+  parse_error = parse_error_save;
+}
+
+static char const *id_type_str(int id) {
+  switch (id) {
+  case 4:
+    return "literal";
+  default:
+    return "unknown";
+  }
+}
+#else
+#define print_octets(...)
+#define id_type_str(...) NULL
+#endif
+
+typedef struct _machine_t {
+  // states
+} Machine;
 
 int main(void) {
   // Packets are represented as a string of octets.
@@ -120,6 +313,31 @@ int main(void) {
     }
   }
   dbgprintf("\n");
+
+  // Parse stuff
+  uint8_t version, id;
+  Bitdex b = {0};
+
+  version = accept_version(&str, &b);
+  id = accept_id(&str, &b);
+
+  if (parse_error) {
+    fprintf(stderr, "parse error\n");
+    abort();
+  } else {
+    dbgprintf("Version %d, packet id %d (%s)\n", (int)version, (int)id,
+              id_type_str(id));
+  }
+
+  Octets literal = accept_literal(&str, &b);
+  if (parse_error != STOP) {
+    fprintf(stderr, "parse_error status %d unexpected\n", parse_error);
+    abort();
+  } else {
+    dbgprintf("Literal accepted\n");
+    print_octets(&literal);
+    free(literal.octets);
+  }
 
   free(str.octets);
 }
